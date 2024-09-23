01f83a5dc93481dffbf6a87dd5a83d9c7858d297
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-09-23 19:33:06 +0530

    testing

--- src/apiURL.js
+++ src/apiURL.js
@@ -1,3 +1,3 @@
-const SHEETYAPI =  "https://api.sheety.co/584e3066e3d830496e2806328c85c0be/pickupdata/sheet1"
-const CANCELSHEETYAPI =  "https://api.sheety.co/584e3066e3d830496e2806328c85c0be/canceledBooking/sheet1"
+const SHEETYAPI =  "https://api.sheety.co/d8d0c1c371101a10b17498a21b56cf51/pickupdata/sheet1"
+const CANCELSHEETYAPI =  "https://api.sheety.co/d8d0c1c371101a10b17498a21b56cf51/pickupdata/sheet1"
 export default  {SHEETYAPI:SHEETYAPI , CANCELSHEETYAPI:CANCELSHEETYAPI}
