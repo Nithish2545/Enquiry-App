1249c70eea49144f1e177a78a4c0b1963209b824
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-10-08 19:05:51 +0530

    testing

--- src/apiURL.js
+++ src/apiURL.js
@@ -1,7 +1,7 @@
 const SHEETYAPI =
-  "https://api.sheety.co/1bd9896e61b80a385dd2aa14c68c9277/pickupdataChennai/sheet1";
+  "https://api.sheety.co/75cc6a982d934c7d4ee40aaf4161d985/pickupdataChennai/sheet1";
 
   const CANCELSHEETYAPI =
-  "https://api.sheety.co/1bd9896e61b80a385dd2aa14c68c9277/canceledBooking/sheet1";
+  "https://api.sheety.co/75cc6a982d934c7d4ee40aaf4161d985/canceledBooking/sheet1";
   
 export default { CHENNAI: SHEETYAPI,  COIMBATORE:SHEETYAPI, PONDY:SHEETYAPI, CANCELSHEETYAPI: CANCELSHEETYAPI };
